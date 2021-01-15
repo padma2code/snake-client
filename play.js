7eac2acb7c5596b6213602fbfb539517cbd636fb
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-15 05:18:53 +0000

    refactored client.js and play.js

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require('net');
+//const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
-connect();
-
+connect()
